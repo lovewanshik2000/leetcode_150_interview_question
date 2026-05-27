65076ac2aa70fba9c95963b47726e369a567539f
Kamlesh Lovewanshi <85796224+lovewanshik2000@users.noreply.github.com>
2026-05-27 17:20:08 +0000

    Bubble sort done

---
+++ learning_topic/sorting/2_bubble_sort.py
@@ -0,0 +1,21 @@
+# Bubble Sort (Adjacent swaps)
+
+def bubble_sort(nums):
+
+    n = len(nums)
+
+    for i in range(n-2, -1,-1):
+        is_swap = False
+        for j in range(0, i+1):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+                is_swap = True
+        if is_swap == False:
+            return nums, is_swap
+    return nums, is_swap
+
+
+# Driver Code
+nums = [5,2,7,3,6,1,4,8,9,0]
+# nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(bubble_sort(nums))
